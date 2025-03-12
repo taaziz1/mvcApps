9085ca7dfb221bb1766ebf475c21144f6c01b1aa
Talha <talha.aziz@sjsu.edu>
2025-03-12 10:31:39 -0700

    Implementation for Publisher and Subscriber

--- src/mvc/Publisher.java
+++ src/mvc/Publisher.java
@@ -1,4 +1,25 @@
 package mvc;
 
-public class Publisher {
+import java.util.ArrayList;
+
+public abstract class Publisher {
+    private ArrayList<Subscriber> subscribers;
+
+    public Publisher() {
+        subscribers = new ArrayList<Subscriber>();
+    }
+
+    public void subscribe(Subscriber s) {
+        subscribers.add(s);
+    }
+
+    public void unsubscribe(Subscriber s) {
+        subscribers.remove(s);
+    }
+
+    public void notifySubscribers() {
+        for(Subscriber s : subscribers) {
+            s.update();
+        }
+    }
 }
--- src/mvc/Subscriber.java
+++ src/mvc/Subscriber.java
@@ -1,4 +1,5 @@
 package mvc;
 
-public class Subscriber {
+public interface Subscriber {
+    void update();
 }
