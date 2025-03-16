73800f8a9313bcff793ce2d780dde2cb5666c895
synzyg <howiepham264@gmail.com>
2025-03-15 19:56:07 -0700

    Updated abstract class Command, interface AppFactory.

--- src/mvc/AppFactory.java
+++ src/mvc/AppFactory.java
@@ -1,4 +1,17 @@
 package mvc;
 
-public interface  AppFactory {
+public interface AppFactory {
+	public Model makeModel();
+	
+	public View makeView(Model model);
+	
+	public String getTitle();
+	
+	public String[] getHelp();
+	
+	public String about();
+	
+	public String[] getEditCommands();
+	
+	public Command makeEditCommand(Model model, String name, Object source);
 }
